3487813e2ffc090ee03cb85868c4723c9e5b0868
Carl Calderon <carl.calderon@gmail.com>
2014-11-14 12:22:09 +0100

    Added comments.

--- bux.hx
+++ bux.hx
@@ -3,11 +3,17 @@ import sys.io.Process;
 import haxe.io.Input;
 import haxe.io.Output;
 
+/**
+ * bux application / instance
+ */
 class Bux
 {
 
-  private static var VERSION :String = "0.0.1";
+  private static var VERSION :String = "0.0.2";
 
+  /**
+   * Application entry
+   */
   static public function main() {
     var bux = new Bux();
     bux.interpret(Sys.args());
@@ -18,10 +24,44 @@ class Bux
   private static var REGEX_REGULAR_EXPRESSION_INPUT :EReg = ~/\/(.+?)\/(\w+?)?/;
   private static var REGEX_COMMAND_REPLACE          :EReg = ~/(?<!\\)\{(\d)(?<!\\)\}/g;
 
+  /**
+   * Attached to the `--dry-run` flag. If `true` the command specified will not
+   * be executed and the argument / command merge will only be traced. This
+   * allows the user to validate the command construction prior to live-run.
+   */
   private var FLAG_DRY_RUN    :Bool = false;
+
+  /**
+   * Attached to the `--lines` flag. If `true`, bux will merge and execute for
+   * each line in STDIN rather than merge with complete STDIN buffer. This is
+   * handy if you for instance want to keep a list of different arguments in a
+   * separate file and execute a cli command for each item in the list.
+   *
+   * Example:
+   *
+   * - capitals.txt
+   * Sweden, Stockholm
+   * Spain, Madrid
+   * France, Paris
+   *
+   * - CL
+   * $ cat capitals.txt | bux -ld "/(\w+),\s?([\w\s]+)/i" "{2} is the capital of {1}"
+   *
+   * - output
+   * Stockholm is the capital of Sweden
+   * Madrid is the capital of Spain
+   * Paris is the capital of France
+   */
   private var FLAG_LINE_INPUT :Bool = false;
 
+  /**
+   * Forked from bux STDIN
+   */
   private var stdin  :Input;
+
+  /**
+   * Forked to bux STDOUT
+   */
   private var stdout :Output;
 
   /**
@@ -31,8 +71,8 @@ class Bux
   private var userRegEx :EReg;
 
   /**
-   * User defined command exec. This is the piece which will be injected with
-   * matches and groups from the user regular expression.
+   * User defined command exec (required). This is the piece which will be
+   * injected with matches and groups from the user regular expression.
    */
   private var commandString :String;
 
@@ -45,6 +85,11 @@ class Bux
     stdin  = Sys.stdin();
   }
 
+  /**
+   * Reads an Array of Strings and assigns the proper flags and arguments to
+   * the bux process.
+   * @param  args :Array<String> List of arguments (typically ARGV)
+   */
   public function interpret(args :Array<String>):Void {
     for (arg in args) {
       if (REGEX_COMMAND_LINE_ARGUMENT.match(arg)) {
@@ -80,6 +125,10 @@ class Bux
     }
   }
 
+  /**
+   * Assigns the proper process flag based on input.
+   * @param  flag :String       A single flag (leading dash `-` omitted)
+   */
   private function registerFlag(flag :String):Void {
     switch (flag) {
       case "v" | "version": printVersion();
@@ -89,6 +138,9 @@ class Bux
     }
   }
 
+  /**
+   * Starts the regular expression and command merge and execution.
+   */
   public function begin():Void {
     var regex   :EReg   = userRegEx;
     var command :String = commandString;
@@ -104,6 +156,14 @@ class Bux
     }
   }
 
+  /**
+   * Merges multiple inputs and returns the combined result.
+   * @param  input   :String       Typically STDIN / line
+   * @param  regex   :EReg         Merge rule (regular expression)
+   * @param  command :String       CL command to merge into
+   * @return         :String|null  If no replacement was performed null will be
+   *                               returned instead.
+   */
   private function merge(input :String, regex :EReg, command :String):String {
     var output     :String = command;
     var foundMatch :Bool   = false;
@@ -127,6 +187,11 @@ class Bux
     return output;
   }
 
+  /**
+   * Executes a CL program. If `--dry-run` flag is specified, command will be
+   * traced to STDOUT instead.
+   * @param  command :String       Command to execute
+   */
   private function exec(command :String):Void {
     if (command == null) {
       return;
@@ -143,12 +208,20 @@ class Bux
     }
   }
 
+  /**
+   * Trace version.
+   * @param  exitCode :UInt         (optional)
+   */
   private function printVersion(exitCode :UInt = 0):Void {
     stdout.writeString(VERSION);
     stdout.flush();
     Sys.exit(exitCode);
   }
 
+  /**
+   * Trace help and usage.
+   * @param  exitCode :UInt         (optional)
+   */
   private function printHelp(exitCode :UInt = 0):Void {
     var information:String = [
       "bux v" + VERSION,
